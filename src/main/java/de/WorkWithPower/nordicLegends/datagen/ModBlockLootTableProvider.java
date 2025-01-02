82899e4a34049487e6d6f0b5f5895c2ac2acca62
Ricco <ricco.hipp@outlook.de>
2025-01-02 17:33:01 +0100

    Added elderwood_planks

--- src/main/java/de/WorkWithPower/nordicLegends/datagen/ModBlockLootTableProvider.java
+++ src/main/java/de/WorkWithPower/nordicLegends/datagen/ModBlockLootTableProvider.java
@@ -4,9 +4,20 @@ import de.WorkWithPower.nordicLegends.block.register.ModBlocks;
 import de.WorkWithPower.nordicLegends.item.ModItems;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 import java.util.Set;
 
@@ -19,14 +30,29 @@ public class ModBlockLootTableProvider extends BlockLootSubProvider {
     protected void generate() {
         dropSelf(ModBlocks.MITHRIL_BLOCK.get());
         dropSelf(ModBlocks.BREWING_BARREL.get());
+        dropSelf(ModBlocks.ELDERWOOD_PLANKS.get());
 
 
         add(ModBlocks.MITHRIL_DEEPSLATE_ORE.get(),
-                block -> createOreDrop(ModBlocks.MITHRIL_DEEPSLATE_ORE.get(), ModItems.RAW_MITHRIL.get()));
+                block -> createMultipleOreDrops(ModBlocks.MITHRIL_DEEPSLATE_ORE.get(), ModItems.RAW_MITHRIL.get(),1,3));
     }
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
         return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+
+    //Adjusted Copy of createCopperOreDrops
+    protected LootTable.Builder createMultipleOreDrops(Block block, Item item, float rMinDrops, float rMaxDrops) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(
+                block,
+                (LootPoolEntryContainer.Builder<?>)this.applyExplosionDecay(
+                        block,
+                        LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(rMinDrops, rMaxDrops)))
+                                .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
     }
 }
